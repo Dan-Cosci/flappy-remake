4f7060fc4ee7eced3a9c932cab1cfa95a924a873
Dan-Cosci <allinasdaniel@gmail.com>
2026-05-08 06:19:29 +0800

    start

---
+++ app/src/main/java/com/example/flappybird/GameSurface.java
@@ -0,0 +1,30 @@
+package com.example.flappybird;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
+    public GameSurface(Context context) {
+        super(context);
+
+        this.getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
